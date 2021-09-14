b6f568a02c05a6493e00c5aa134bf43377662c71
marcgalitski <marc.galitski@gmail.com>
2021-09-14 23:10:50 +0200

    Save progress

--- crossover.py
+++ crossover.py
@@ -5,18 +5,33 @@ class Crossover:
     # This class contains diffrent crossover implementations
 
     # Keep in mind that offspring count CANNOT exceed popualtion count
-    offspring_ratio = 1.5
+
+    """
+    Implements different crossover variations
+
+
+    """
+
+    def __init__(self, offspring_ratio):
+
+        self.offspring_ratio = 1.5
 
     @staticmethod
-    def basic(parents):
-        # This implementation creates offspring_ratio times number of parents childred
-        # Child consists of 0 to crossing point father genes, the rest are mother's genes
+    def basic(self, parents):
+        """
+        Implements basic crossover method by using the offspring_ratio times the number of parents children
+        
+        Child is the sum of father genome (0:crossing_point) and mother genome (offspring_point:)
+        Params
+        ------
+        parents : list of 2 parents for the offspring
+        """
 
         parents_count = parents.shape[0]
         genome_length = parents.shape[1]
 
         offspring = np.array([])
-        offspring_count = round(Crossover.offspring_ratio * parents_count)
+        offspring_count = round(self.offspring_ratio * parents_count)
 
         for i in range(offspring_count):
             mother_index = np.random.randint(parents_count)
