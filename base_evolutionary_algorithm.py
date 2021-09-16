Base_algo: fixed generation iteratable (increased at beginning & end, instead of just end); experiment.py: fixed calculation of total average - successfully plots total average over all generations in a single experiment against experiment number; specialist: changed some hyperparams

base_evolutionary_algorithm.py
@@ -38,7 +38,6 @@ class EvolutionaryAlgorithm:
         generation = 1
 
         while(generation <= self.generations_number):
-            generation += 1
             # fitness is an array of fitnesses of individuals.
             # fitness[i] is a fitness of population[i]
             fitness = self.get_fitness()
@@ -61,13 +60,14 @@ class EvolutionaryAlgorithm:
             if DEBUG:
                 print(f'Population shape: {self.population.shape}')
                 print(f'Current Generation {generation}; Best fitness: {self.best_fitness}')
-                print(f'Best Solution: {self.best, self.best.shape}')
+                # print(f'Best Solution: {self.best, self.best.shape}')
 
             # INCREMENT GENERATION
             generation += 1
 
             # CALCULATE AVERAGE FITNESS FOR GENERATION
             avg_generation_fitness = np.append(avg_generation_fitness, np.average(fitness))
+            
 
         return self.best, self.best_fitness, avg_generation_fitness
 

experiment.py
@@ -21,11 +21,11 @@ class Experiment:
 
         for i in range(experiments):
             best, best_fitness, avg_generation_fitness = self.evolutionary_algorithm.run()
-            avg_fitness_gen = np.append(avg_fitness_gen, avg_generation_fitness)
+            avg_fitness_gen = np.append(avg_fitness_gen, np.average(avg_generation_fitness)) # assign average of average fitness to avg_fitness_gen
             self.best_solutions = np.append(self.best_solutions, best)
             self.best_solutions_fitness = np.append(self.best_solutions_fitness, best_fitness)
 
-            if DEBUG: print(f'EXPERIMENT NUMBER {i+1}: Average generation fitness: {avg_fitness_gen, avg_fitness_gen.shape} \n\n Best solutions {self.best_solutions, self.best_solutions.shape} \n Fitness of the best solutions {self.best_solutions_fitness}')
+            if DEBUG: print(f'EXPERIMENT NUMBER {i+1}: Average generation fitness: {avg_fitness_gen, avg_fitness_gen.shape} \n\n\n Fitness of the best solutions {self.best_solutions_fitness}')
         # Plot the results of all experimental runs
         self.line_plot(avg_fitness_gen, experiments)
 
@@ -38,8 +38,7 @@ class Experiment:
 
         """
         # 1. ASSIGN ARRAY OF AVERAGE FITNESSES OVER ALL GENERATIONS OVER EACH EXPERIMENT TO DATAPOINTS
-        average_of_average = np.divide(average_fitness_generation, num_experiments)
-        data_points = np.array(average_of_average)
+        data_points = average_fitness_generation
 
         # 2. FILL ARRAY WITH VALUES CORRESPONDING TO NUMBER OF EXPERIMENTS
         array_experiments = np.array([i+1 for i in range(num_experiments)]) # list comprehension