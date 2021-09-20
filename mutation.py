721b189fe370497c1424b5939fb2cd7dd232c1a4
marcgalitski <marc.galitski@gmail.com>
2021-09-20 18:01:18 +0200

    Added uniform_mutation, made changes to mutation_selection params & print message

--- mutation.py
+++ mutation.py
@@ -29,3 +29,24 @@ class Mutation:
             mutants = np.concatenate((mutants, mutant), axis=None)
 
         return mutants.reshape(selected_group_count, genome_length)
+
+
+
+    def uniform_mutation(selected_group):
+        genome_length = selected_group.shape[1]
+        mutated_genes_count = round(Mutation.mutation_ratio * genome_length) # 0.1 * 105 = 10.5 = 11
+
+        selected_group_count = selected_group.shape[0] # how many individuals are selected for mutation
+        mutants = np.array([])
+
+        for i in range(selected_group_count):
+            mutant = selected_group[np.random.randint(selected_group_count)]
+
+            for j in range(mutated_genes_count):
+                gene_index = np.random.randint(genome_length)
+                # DRAW A RANDOM VALUE FROM A UNIFORM DISTRIBUTION 
+                mutant[gene_index] = np.random.uniform(-1.0, 1.0)
+
+            mutants = np.concatenate((mutants, mutant), axis=None)
+
+        return mutants.reshape(selected_group_count, genome_length)
