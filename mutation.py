42ba0593bc8c16bdf9a9c8d9b45a7d6e1effc91e
dymitrlubczyk <56291334+dymitrlubczyk@users.noreply.github.com>
2021-09-29 16:57:52 +0200

    🔷 Gaussian mutation (#18)

--- mutation.py
+++ mutation.py
@@ -6,7 +6,7 @@ class Mutation:
         Performs the mutation on the individuals in the Mating set
 
 
-        Mutation ratio shows highest 
+        Mutation ratio shows highest
         Params:
         -------
         mutation_ratio : set by specialist.py
@@ -36,8 +36,6 @@ class Mutation:
         selected_group_count = selected_group.shape[0]
         mutants = np.array([])
 
-
-
         for i in range(selected_group_count):
             mutant = selected_group[np.random.randint(selected_group_count)]
 
@@ -63,6 +61,25 @@ class Mutation:
             for j in range(mutated_genes_count):
                 gene_index = np.random.randint(genome_length)
                 mutant[gene_index] = np.random.uniform(-1.0, 1.0)
+
+            mutants = np.concatenate((mutants, mutant), axis=None)
+
+        return mutants.reshape(selected_group_count, genome_length)
+
+    def gaussian_mutation(selected_group):
+        sigma = 0.3
+        genome_length = selected_group.shape[1]
+        mutated_genes_count = round(Mutation.mutation_ratio * genome_length)
+
+        selected_group_count = selected_group.shape[0]
+        mutants = np.array([])
+
+        for i in range(selected_group_count):
+            mutant = selected_group[np.random.randint(selected_group_count)]
+
+            for j in range(mutated_genes_count):
+                gene_index = np.random.randint(genome_length)
+                mutant[gene_index] += min(max(np.random.normal(0, sigma), -1), 1)
 
             mutants = np.concatenate((mutants, mutant), axis=None)
 
