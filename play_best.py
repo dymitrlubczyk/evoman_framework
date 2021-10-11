9e653964c0129d688fa977b0b9ddfaf9584cca30
Adrian S.A <44787687+maplesyrup18@users.noreply.github.com>
2021-10-11 15:11:10 +0200

    Update play_best.py

--- play_best.py
+++ play_best.py
@@ -72,6 +72,6 @@ class PlayBest:
                     if(EXTRA_DETAILS == True):
                         print("Player life: ", temp_player_life, " -- Enemy life: ",
                             temp_enemy_life, " -- Time: ", temp_time)
-        avg_player_life_all = np.append(avg_player_life_all, np.mean(avg_player_life_1enemy))
-        avg_enemy_life_all = np.append(avg_enemy_life_all, np.mean(avg_enemy_life_1enemy))
+            avg_player_life_all = np.append(avg_player_life_all, np.mean(avg_player_life_1enemy))
+            avg_enemy_life_all = np.append(avg_enemy_life_all, np.mean(avg_enemy_life_1enemy))
         
